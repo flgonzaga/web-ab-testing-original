c54abf2df166b128b2e36bdb668725eed084e792
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-21 09:54:07 -0300

    test: google optimize

--- optimize.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Sets the ID of the experiment on variants of this web page.
-$experimentId = '3uvylVSkSv-aNskBLmFwRw';
-
-// Randomly picks a variant for the user.
-$variationId = 0; //rand(0, 1);
-?>
