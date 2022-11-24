5db3c44663e0ffde0253118ffb2a98bfadb5ce6b
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-24 13:25:33 -0300

    test: modify time x-ms-routing-name cookie

--- test.php
+++ test.php
@@ -1,11 +1,10 @@
 <?php
 
-if (isset($_COOKIE['x-ms-routing-name']))
-{
+// if (isset($_COOKIE['x-ms-routing-name'])) {
     $current_cookie_value = $_COOKIE['x-ms-routing-name'];
     setcookie("x-ms-routing-name", $current_cookie_value, strtotime( '+365 days' ), "/", "." . $_SERVER['SERVER_NAME'], false, false);
     setcookie("x-ms-routing-name-test", $current_cookie_value, strtotime( '+365 days' ), "/", "." . $_SERVER['SERVER_NAME'], false, false);
-}
+// }
 
 ?>
 <html lang="en">
