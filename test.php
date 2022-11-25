5f0355b444e565d76cb83b5aa9467e3ce17df1a3
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-25 13:07:10 -0300

    test: try change life time of cookie using php

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 if (isset($_COOKIE['x-ms-routing-name'])) {
     $current_cookie_value = $_COOKIE['x-ms-routing-name'];
 
-    // setcookie("x-ms-routing-name", $current_cookie_value, strtotime( '+365 days' ), "/", "." . $_SERVER['SERVER_NAME'], false, false);
-    // setcookie("x-ms-routing-name-test", $current_cookie_value, strtotime( '+365 days' ), "/", "." . $_SERVER['SERVER_NAME'], false, false);
+    setcookie("x-ms-routing-name", $current_cookie_value, strtotime( '+365 days' ), "/", "." . $_SERVER['SERVER_NAME'], false, false);
+    setcookie("x-ms-routing-name-test", $current_cookie_value, strtotime( '+365 days' ), "/", "." . $_SERVER['SERVER_NAME'], false, false);
     
     echo $current_cookie_value;
 }
